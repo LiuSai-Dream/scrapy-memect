4fb35923e522771f0d8afe26b08daa01e583dda3
LiuSai-Dream <1liusai253@163.com>
2016-07-03 16:24:33 +0800

    Alter android

--- memect/androidpnPush.py
+++ memect/androidpnPush.py
@@ -9,10 +9,18 @@ Created on Sun Jul 03 00:27:13 2016
 from selenium import webdriver
 import logging
 import sys
+import MySQLdb
+from  memect.settings import MySqlConfig
+from datetime import datetime, date
+import gc
+import random
+from memect.constants import *
 
 logger = logging.getLogger(__name__)
-
 url = "http://127.0.0.1:8080/notification.do"
+cur = None
+conn = None
+curDate = datetime.now().date().strftime("%Y-%m-%d")
 
 def setNotification(driver, titleText, messageText, uriText):
     logger.debug("title : "+ titleText)    
@@ -35,7 +43,6 @@ def setNotification(driver, titleText, messageText, uriText):
 def submitAndClose(driver):
     submit = driver.find_element_by_xpath("//input[contains(@type,'submit')]")
     submit.submit()
-
     driver.close()
 
 class safeList(list):
@@ -44,17 +51,75 @@ class safeList(list):
             return self.__getitem__(index)
         except IndexError:            
             return default
-            
-def main(args):
-    sl = safeList(args)
+        
+def connectMysql():
+    user = MySqlConfig['user'],
+    passwd = MySqlConfig['passwd'],
+    dbname = MySqlConfig['db'],
+    host = MySqlConfig['host'],
+    charset = "utf8",
+    use_unicode = True
     
+    try:
+        # Exceptions make it extremely difficult to clean up bu hand.
+        # Manually invok python garbage collector 
+        # which close off any old Mysql connections
+        gc.collect()        
+        
+        # Connect to mysql database
+        conn = MySQLdb.connect(user = user, passwd = passwd, db = dbname, host = host , charset = charset, use_unicode = use_unicode)
+		
+        # All operations are performed in the cursor
+        cur = conn.cursor()
+        logger.debug('Debug: Connecting to database successfully')
+    except :
+        if (conn):
+            conn.close()  
+
+def getCurDateResult():
+    query = """SELECT ( author_name, author_img_url, author_page_url, pub_time, keywords, content_text, content_page_url,  content_img_url) FROM (%s) WHERE pub_time LIKE (%s) """    
+    tables = ["ml_memct", "bd_memect", "app_memect", "web_memect", "py_memect"]
+    results = []
+        
+    for table in tables:
+        resultNum = cur.execute(query, (table, curDate))
+        if (resultNum != 0):
+            result = cur.fetchAll()
+            print(result)
+    else:
+        logger.warn("Warning: Table ")
+    return results
+    
+def sendNotification(results):
+    sl = None
+    if (len(results) <= 0):
+        sl = safeList([])
+    else:
+        index = random.randint(0, len(results))
+        sl = safeList(results[index])        
+    
+    # Trigger an request
     driver = webdriver.PhantomJS('phantomjs')
     driver.get(url)
-    
-    setNotification(driver, sl.get(0), sl.get(1), sl.get(2))
+    setNotification(driver, sl.get(0), sl.get(5), sl.get(6))
     submitAndClose(driver)
+    
+def closeMysql():
+    if (conn):
+        conn.close()
+        
+def main():
+    connectMysql()
+    if (conn == None):
+        logger.error("Error: Fail to connect mysql")
+        return 
+        
+    results = getCurDateResult()
+    sendNotification(results)
+    closeMysql()
+    
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
     
     
--- memect/create_table.sql
+++ memect/create_table.sql
@@ -3,7 +3,7 @@ CREATE DATABASE `memect` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 use memect;
 
 CREATE TABLE ml_memect ( 
-    ml_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     author_name VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_general_ci,
     author_img_url VARCHAR(500),
     author_page_url VARCHAR(500),
@@ -12,11 +12,11 @@ CREATE TABLE ml_memect (
     content_text VARCHAR(1500) CHARACTER SET utf8 COLLATE utf8_general_ci, 
     content_img_url VARCHAR(500),
     content_page_url VARCHAR(500),
-    PRIMARY KEY(ml_id)
+    PRIMARY KEY(id)
     );
 
 CREATE TABLE py_memect ( 
-    ml_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     author_name VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_general_ci,
     author_img_url VARCHAR(500),
     author_page_url VARCHAR(500),
@@ -25,11 +25,11 @@ CREATE TABLE py_memect (
     content_text VARCHAR(1500) CHARACTER SET utf8 COLLATE utf8_general_ci,
     content_img_url VARCHAR(500),
     content_page_url VARCHAR(500),
-    PRIMARY KEY(ml_id)
+    PRIMARY KEY(id)
     );
 
 CREATE TABLE web_memect ( 
-    ml_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     author_name VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_general_ci,
     author_img_url VARCHAR(500),
     author_page_url VARCHAR(500),
@@ -38,11 +38,11 @@ CREATE TABLE web_memect (
     content_text VARCHAR(1500) CHARACTER SET utf8 COLLATE utf8_general_ci,
     content_img_url VARCHAR(500),
     content_page_url VARCHAR(500),
-    PRIMARY KEY(ml_id)
+    PRIMARY KEY(id)
     );
 
 CREATE TABLE app_memect ( 
-    ml_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     author_name VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_general_ci,
     author_img_url VARCHAR(500),
     author_page_url VARCHAR(500),
@@ -51,11 +51,11 @@ CREATE TABLE app_memect (
     content_text VARCHAR(1500) CHARACTER SET utf8 COLLATE utf8_general_ci,
     content_img_url VARCHAR(500),
     content_page_url VARCHAR(500),
-    PRIMARY KEY(ml_id)
+    PRIMARY KEY(id)
     );
 
 CREATE TABLE bd_memect ( 
-    ml_id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     author_name VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_general_ci,
     author_img_url VARCHAR(500),
     author_page_url VARCHAR(500),
@@ -64,7 +64,7 @@ CREATE TABLE bd_memect (
     content_text VARCHAR(1500) CHARACTER SET utf8 COLLATE utf8_general_ci,
     content_img_url VARCHAR(500),
     content_page_url VARCHAR(500),
-    PRIMARY KEY(ml_id)
+    PRIMARY KEY(id)
     );
 
 CREATE TABLE sha1url ( 
