c5ef370eb662e818f2f033ce5281c2fb230a1596
LiuSai-Dream <1liusai253@163.com>
2016-07-03 09:48:11 +0800

    Add crawl date pickle

--- memect/spiders/spider.py
+++ memect/spiders/spider.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 import logging
+import pickle
 from scrapy.http import Request
 from scrapy.spiders import CrawlSpider, Rule
 from memect.items import MemectItem
@@ -15,6 +16,9 @@ class MlmemectSpider(CrawlSpider):
 	name = "memect"
 	allow_domain = ['ml.memect.com/', 'py.memect.com/', 'web.memect.com/', 'bd.memect.com/', 'app.memect.com/']
 	start_urls = ['http://ml.memect.com/', 'http://py.memect.com/', 'http://web.memect.com/', 'http://bd.memect.com/', 'http://app.memect.com/']
+	global crawl_date = None
+	crawl_date_file = "crawl_date.pkl"
+
 
 	# Instance varibales
 	def __init__(self):
@@ -23,10 +27,11 @@ class MlmemectSpider(CrawlSpider):
 		self.keywords = "keywords"
 		self.content_text = "text"
 		self.content_img_url = "original_pic"
-		
+		getPickleDate()
+
 
 	def parse(self, response):
-		logger.debug("The url is " + response.url)
+		logger.debug("Parsing " + response.url)
 		siteType = ""
 		if response.url == self.start_urls[0] :
 			siteType = ML
@@ -44,21 +49,74 @@ class MlmemectSpider(CrawlSpider):
 				yield Request(url = link, callback = self.parse_content, meta = {'siteType' : siteType})
 		#	yield Request(url = response.xpath("//a[contains(@href, 'long')]/@href").extract_first(), callback = self.parse_content)
 		else:
-			logger.error("..............................................Fail to fetch url : " + response.url + "..............................................")
+			logger.error(".........Error: Fail to fetch " + response.url + ".........")
+
+
+	def getPickleDate(self):
+		try:
+			with open(crawl_date_file, "rb") as handle:
+				crawl_date = pickle.load(handle)
+			logger.debug(".........Loading pickfile " + crawl_date_file + " successfully !  " + crawl_date + ".........")
+		except:
+			crawl_date = {ML:"2016-06-13", PY:"2016-06-13", ML:"2016-06-13", BD:"2016-06-13", APP:"2016-06-13"}
+			logger.error(".........Error: loading pickfile " + crawl_date_file + "; use default date.........")
+	
+
+	def savePickleDate(self):
+		try:
+			with open(crawl_date_file, "wb") as handle:
+				pickle.dump(crawl_date, handle)
+			logger.debug(".........Dumping pickfile " + crawl_date_file + " successfully !  " + crawl_date + ".........")
+		except:
+			logger.error(".........Error: dumping pickfile " + crawl_date_file + ".........")
+
+
+	def need_crawl(self, siteType, curDate):
+		ret = False
+		if siteType == ML :
+			if crawl_date[ML] > curDate:
+				ret = True 
+				crawl_date[ML] = curDate
+		elif siteType == PY:
+			if crawl_date[PY] > curDate:
+				ret = True
+				crawl_date[PY] = curDate
+		elif siteType == WEB:
+			if crawl_date[WEB] > curDate:
+				ret = True
+				crawl_date[WEB] = curDate
+		elif siteType == BD:
+			if crawl_date[BD] > curDate:
+				ret = True
+				crawl_date[BD] = curDate
+		elif siteType == APP:
+			if crawl_date[APP] > curDate:
+				ret = True
+				crawl_date[APP] = curDate
+		if ret:
+			savePickleDate()
+		return ret
 
 	def parse_content(self, response):
-		logger.debug("The url is " + response.url)
+		logger.debug("Parsing: " + response.url)
 		if (response.status != 200):
-			logger.error("..............................................Fail to fetch url : " + response.url +"..............................................")
+			logger.error(".........Error: Fail to fetch url!  " + response.url +".........")
 			return
+
+		siteType = response.meta['siteType']
 
 		# Getting the date, for selecting thread next
 		curDate = response.xpath('//body//span[contains(@id, "date")]/text()').extract_first()
 
 		# Fail to get curdate
 		if curDate == None:
-			logger.error("..............................................Fail to extract curDate!  The url is " + response.url + "..............................................")
+			logger.error(".........Error: Fail to extract curDate! " + response.url + ".........")
 			return
+
+		# Determine weather to crawl by comparing date
+		if ( not need_crawl(siteType, curDate)):
+				logger.debug("Date " + curDate + " need not to crawl!")
+				return
 
 		# Setting filter date, for xpath selecting next
 		filterDate = "day_" + curDate
@@ -66,7 +124,6 @@ class MlmemectSpider(CrawlSpider):
 		# Getting all threads of day curDate
 		threadsSelectors = response.xpath('//body//div[contains(@class, "'+filterDate+'")]')
 
-		siteType = response.meta['siteType']
 		# Interating all threads
 		for thread in threadsSelectors:
 		# for thread in threadsSelectors[1:5]:
@@ -112,14 +169,14 @@ class MlmemectSpider(CrawlSpider):
 				content_text  = content_text.strip() + " "
 		        
 			except AttributeError as e:
-				logger.debug (" ..............................................Exception in parsing content_text , Exception is  " + str(e)  + "..............................................")
+				logger.debug (" .........Exception: parsing content_text; " + str(e)  + ".........")
 			else :
 				try:
 					content_text_href = thread.xpath('.//div[contains(@class, "'+self.content_text+'")]//a/text()').extract_first()                           
 					content_text = content_text + content_text_href.strip()	
 					item['content_text'] = content_text
 				except AttributeError as e:
-					logger.debug ( "..............................................Exception in parsing content_url " + str(e) + ".............................................." ) 
+					logger.debug ( ".........Exception: parsing content_url; " + str(e) + "........." ) 
 
 			# Getting @{content_img_url}
 			item['content_img_url'] = thread.xpath('.//div[contains(@class, "'+self.content_img_url +'")]/a/@href').extract_first()
